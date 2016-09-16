6cd784fe06d1362b8a5899da2b7e45dee98dcb26
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-09-15 17:37:02 -0700

    Added 9-15 zip

---
+++ Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var variable = 10
+variable = 0
+let constant = 10
+// constant = 0 – Error because this is a constant and you can't reassign
+
+for i in 0..<10 {
+    // for 0 througn 9 (because less than 10) run this...
+    // ...
+}
+
+func functionName(param1: String, param2: String) -> String {
+    // Put what you want your function to do
+    return "return value"
+}
+
+print(functionName(param1: "String", param2: "String"))
